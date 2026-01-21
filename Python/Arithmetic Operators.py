e61356c971aabf2af13025baf692bfb08650dc1f
daxpatel235 <pateldax23056@gmail.com>
2026-01-22 00:00:54 +0530

    Add files via upload

---
+++ Python/Arithmetic Operators.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input("enter value of a: "))
+    b = int(input("enter value of b: "))
+    
+    print("the sum of a and b is", a+b)
+    print("the subtraction of a and is",a-b)
+    print("the multiplication of a and b is",a*b)
+
